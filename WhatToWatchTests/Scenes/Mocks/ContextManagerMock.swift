6da259a5c8f858cb8215c40984e3e62874edbcf1
Francisco Dzuryk <francisco.dzurky@rappi.com>
2019-05-26 22:25:03 -0300

    Fixint Movie Controller Tests.

--- WhatToWatchTests/Scenes/Mocks/ContextManagerMock.swift
+++ WhatToWatchTests/Scenes/Mocks/ContextManagerMock.swift
@@ -11,6 +11,10 @@ import CoreData
 @testable import WhatToWatch
 
 class ContextManagerMock : ContextManagerInterface {
+    func getContext() -> NSManagedObjectContext {
+        return contextManager.getContext()
+    }
+    
     
     var saveConfigurationCount = 0
     var getConfigurationCount = 0
@@ -27,8 +31,26 @@ class ContextManagerMock : ContextManagerInterface {
     var returnGetMovies = false
     var returnGetShows = false
     
-    func getContext()  -> NSManagedObjectContext {
-        return NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    var contextManager: ContextManager!
+    
+    init() {
+        let bnl = Bundle(for: AppDelegate.self)
+        guard let modelURL = bnl.url(forResource: "WhatToWhatchModel", withExtension: "momd"),
+            let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
+            else {
+                fatalError("Unable to located Core Data model")
+        }
+        
+        let container = NSPersistentContainer(name: "WhatToWhatchModel", managedObjectModel: managedObjectModel)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.persistentStoreCoordinator = container.persistentStoreCoordinator
+        contextManager = ContextManager(testSotreCoordinator: container, testContext: context)
     }
     
     func saveContext() {
@@ -45,11 +67,22 @@ class ContextManagerMock : ContextManagerInterface {
     
     func getConfigurationModel() -> APIConfigurationModel? {
         getConfigurationCount += 1
-        return nil
+        if returnConfiguration {
+            let entity = NSEntityDescription.insertNewObject(forEntityName: "APIConfigurationModel", into: getContext()) as! APIConfigurationModel
+            entity.popularMoviesPage = 1
+            entity.upcomingPageCounter = 1
+            entity.topRatedPageCounter = 1
+            entity.popularShowsPage = 1
+            entity.topRatedShowPage = 1
+            entity.onTheAirShowPage = 1
+            return entity
+        } else {
+            return nil
+        }
     }
     
     func updateConfigurationPage(page: Int, forMovieModel model: MoviesPersistenModel) {
-        
+        updateConfigurationPageMoviesCount += 1
     }
     
     func updateConfigurationPage(page: Int, forShowModel model: ShowsPersistenModel) {
@@ -61,7 +94,7 @@ class ContextManagerMock : ContextManagerInterface {
     }
     
     func saveMovies(movies: [Movie], onModel model: MoviesPersistenModel) {
-        
+        saveMoviesCount += 1
     }
     
     func getMovieById(id:Int, fromModel model: MoviesPersistenModel) -> MovieModel? {
@@ -69,11 +102,16 @@ class ContextManagerMock : ContextManagerInterface {
     }
     
     func getMovies(fromModel model:MoviesPersistenModel) -> [MovieModel] {
-        return []
+        getMoviesCount += 1
+        if returnGetMovies {
+            return getMockMoviesArray()
+        } else {
+            return []
+        }
     }
     
     func saveImageForMovieId(_ id:Int, image:UIImage, type:ImageType) {
-        
+        saveImageForMovieIdCount += 1
     }
     
     func cleanShowModel() {
@@ -94,5 +132,24 @@ class ContextManagerMock : ContextManagerInterface {
     
     func saveImageForShowId(_ id:Int, image:UIImage, type:ImageType) {
         
+    }
+    
+    private func getMockMoviesArray() -> [MovieModel] {
+        
+        let movieModel = NSEntityDescription.insertNewObject(forEntityName: "MoviePopularModel", into: getContext()) as! MovieModel
+        movieModel.id = Int32(1)
+        movieModel.title = "movie.title"
+        movieModel.adult = false
+        movieModel.backdropPath = "movie.backdropPath"
+        movieModel.overview = "movie.overview"
+        movieModel.popularity = 3.3
+        movieModel.posterPath = "movie.posterPath"
+        movieModel.releaseDate = "2018-11-22"
+        movieModel.voteAverage = 3.3
+        movieModel.voteCount = Int32(11)
+        movieModel.originalTitle = "movie.originalTitle"
+        movieModel.originalLanguage = "en-US"
+        
+        return [movieModel]
     }
 }
--- WhatToWatchTests/Scenes/Movies/MoviesCtrlerTest.swift
+++ WhatToWatchTests/Scenes/Movies/MoviesCtrlerTest.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class MoviesCtrlerTest: XCTestCase {
 
-    let persistenceManager: ContextManagerInterface? = nil
+    let contextManager: ContextManagerInterface? = nil
     let apiClient: APIClientInterface? = nil
     let view: MoviesVCDelegate? = nil
 
@@ -31,190 +31,190 @@ class MoviesCtrlerTest: XCTestCase {
         XCTAssert(apiClient.getConfigurationCallCount == 1)
     }
 
-//    func testInitWithConfig() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnConfiguration = true
-//        let apiClient = APIClientMock()
-//        _ = MoviesCtrler(MoviesViewMock(), persistenceManager: persistenceManager, apiClient: apiClient)
-//        XCTAssert(persistenceManager.getConfigurationCount == 1)
-//        XCTAssert(persistenceManager.saveConfigurationCount == 0)
-//        XCTAssert(apiClient.getConfigurationCallCount == 0)
-//    }
-//
-//    func testPopularLoadMoviesNoData() {
-//        let persistenceManager = PersistenceManagerMock()
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .popular)
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 2)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 1)
-//        XCTAssert(persistenceManager.saveMoviesCount == 1)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getPopularMoviesCount == 1)
-//    }
-//
-//    func testPopularLoadMovies() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .popular)
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 1)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 0)
-//        XCTAssert(persistenceManager.saveMoviesCount == 0)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getPopularMoviesCount == 0)
-//    }
-//
-//    func testPopularLoadNextPageMovies() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .popular)
-//        ctrler.loadNextPageMovies()
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 2)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 1)
-//        XCTAssert(persistenceManager.saveMoviesCount == 1)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getPopularMoviesCount == 1)
-//    }
-//
-//    func testUpcomingLoadMoviesNoData() {
-//        let persistenceManager = PersistenceManagerMock()
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .upcoming)
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 2)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 1)
-//        XCTAssert(persistenceManager.saveMoviesCount == 1)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getUpcomingMoviesCount == 1)
-//    }
-//
-//    func testUpcomingLoadMovies() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .upcoming)
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 1)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 0)
-//        XCTAssert(persistenceManager.saveMoviesCount == 0)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getUpcomingMoviesCount == 0)
-//    }
-//
-//    func testUpcomingLoadNextPageMovies() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .upcoming)
-//        ctrler.loadNextPageMovies()
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 2)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 1)
-//        XCTAssert(persistenceManager.saveMoviesCount == 1)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getUpcomingMoviesCount == 1)
-//    }
-//
-//    func testTopRatedLoadMoviesNoData() {
-//        let persistenceManager = PersistenceManagerMock()
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .topRated)
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 2)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 1)
-//        XCTAssert(persistenceManager.saveMoviesCount == 1)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getTopRatedMoviesCount == 1)
-//    }
-//
-//    func testTopRatedLoadMovies() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .topRated)
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 1)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 0)
-//        XCTAssert(persistenceManager.saveMoviesCount == 0)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getTopRatedMoviesCount == 0)
-//    }
-//
-//    func testTopRatedLoadNextPageMovies() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//        ctrler.loadMovies(forModel: .topRated)
-//        ctrler.loadNextPageMovies()
-//
-//        XCTAssert(persistenceManager.getMoviesCount == 2)
-//        XCTAssert(persistenceManager.updateConfigurationPageMoviesCount == 1)
-//        XCTAssert(persistenceManager.saveMoviesCount == 1)
-//        XCTAssert(moviesView.updateMoviesCount == 1)
-//        XCTAssert(apiClient.getTopRatedMoviesCount == 1)
-//    }
-//
-//    func testGetImageForMovie() {
-//        let persistenceManager = PersistenceManagerMock()
-//        persistenceManager.returnGetMovies = true
-//        let apiClient = APIClientMock()
-//        let moviesView = MoviesViewMock()
-//        let ctrler = MoviesCtrler(moviesView, persistenceManager: persistenceManager, apiClient: apiClient)
-//
-//        let movie = MoviesVM(managedObject: persistenceManager.getMovies(fromModel: .popular).first!)
-//        ctrler.getImageForMovie(movie: movie, indexPath: IndexPath(row: 0, section: 0))
-//
-//        XCTAssert(persistenceManager.saveImageForMovieIdCount == 1)
-//        XCTAssert(apiClient.getImageCout == 1)
-//        XCTAssert(moviesView.updateImageCount == 1)
-//    }
+    func testInitWithConfig() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnConfiguration = true
+        let apiClient = APIClientMock()
+        _ = MoviesCtrler(MoviesViewMock(), contextManager: contextManager, apiClient: apiClient)
+        XCTAssert(contextManager.getConfigurationCount == 1)
+        XCTAssert(contextManager.saveConfigurationCount == 0)
+        XCTAssert(apiClient.getConfigurationCallCount == 0)
+    }
+
+    func testPopularLoadMoviesNoData() {
+        let contextManager = ContextManagerMock()
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .popular)
+
+        XCTAssert(contextManager.getMoviesCount == 2)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 1)
+        XCTAssert(contextManager.saveMoviesCount == 1)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getPopularMoviesCount == 1)
+    }
+
+    func testPopularLoadMovies() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .popular)
+
+        XCTAssert(contextManager.getMoviesCount == 1)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 0)
+        XCTAssert(contextManager.saveMoviesCount == 0)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getPopularMoviesCount == 0)
+    }
+
+    func testPopularLoadNextPageMovies() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .popular)
+        ctrler.loadNextPageMovies()
+
+        XCTAssert(contextManager.getMoviesCount == 2)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 1)
+        XCTAssert(contextManager.saveMoviesCount == 1)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getPopularMoviesCount == 1)
+    }
+
+    func testUpcomingLoadMoviesNoData() {
+        let contextManager = ContextManagerMock()
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .upcoming)
+
+        XCTAssert(contextManager.getMoviesCount == 2)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 1)
+        XCTAssert(contextManager.saveMoviesCount == 1)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getUpcomingMoviesCount == 1)
+    }
+
+    func testUpcomingLoadMovies() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .upcoming)
+
+        XCTAssert(contextManager.getMoviesCount == 1)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 0)
+        XCTAssert(contextManager.saveMoviesCount == 0)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getUpcomingMoviesCount == 0)
+    }
+
+    func testUpcomingLoadNextPageMovies() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .upcoming)
+        ctrler.loadNextPageMovies()
+
+        XCTAssert(contextManager.getMoviesCount == 2)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 1)
+        XCTAssert(contextManager.saveMoviesCount == 1)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getUpcomingMoviesCount == 1)
+    }
+
+    func testTopRatedLoadMoviesNoData() {
+        let contextManager = ContextManagerMock()
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .topRated)
+
+        XCTAssert(contextManager.getMoviesCount == 2)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 1)
+        XCTAssert(contextManager.saveMoviesCount == 1)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getTopRatedMoviesCount == 1)
+    }
+
+    func testTopRatedLoadMovies() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .topRated)
+
+        XCTAssert(contextManager.getMoviesCount == 1)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 0)
+        XCTAssert(contextManager.saveMoviesCount == 0)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getTopRatedMoviesCount == 0)
+    }
+
+    func testTopRatedLoadNextPageMovies() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+        ctrler.loadMovies(forModel: .topRated)
+        ctrler.loadNextPageMovies()
+
+        XCTAssert(contextManager.getMoviesCount == 2)
+        XCTAssert(contextManager.updateConfigurationPageMoviesCount == 1)
+        XCTAssert(contextManager.saveMoviesCount == 1)
+        XCTAssert(moviesView.updateMoviesCount == 1)
+        XCTAssert(apiClient.getTopRatedMoviesCount == 1)
+    }
+
+    func testGetImageForMovie() {
+        let contextManager = ContextManagerMock()
+        contextManager.returnGetMovies = true
+        let apiClient = APIClientMock()
+        let moviesView = MoviesViewMock()
+        let ctrler = MoviesCtrler(moviesView, contextManager: contextManager, apiClient: apiClient)
+
+        let movie = MoviesVM(movieModel: contextManager.getMovies(fromModel: .popular).first!)
+        ctrler.getImageForMovie(movie: movie, indexPath: IndexPath(row: 0, section: 0))
+
+        XCTAssert(contextManager.saveImageForMovieIdCount == 1)
+        XCTAssert(apiClient.getImageCout == 1)
+        XCTAssert(moviesView.updateImageCount == 1)
+    }
 }
 
-//class MoviesViewMock: MoviesVCDelegate {
-//    var updateMoviesCount = 0
-//    var updateImageCount = 0
-//    
-//    func networkError(error: Error) {
-//        
-//    }
-//    
-//    func updateMovies(movies: [MoviesVM]) {
-//        updateMoviesCount += 1
-//    }
-//    
-//    func updateImage(image: UIImage, indexPath: IndexPath) {
-//        updateImageCount += 1
-//    }
-// 
-//}
+class MoviesViewMock: MoviesVCDelegate {
+    var updateMoviesCount = 0
+    var updateImageCount = 0
+    
+    func networkError(error: Error) {
+        
+    }
+    
+    func updateMovies(movies: [MoviesVM]) {
+        updateMoviesCount += 1
+    }
+    
+    func updateImage(image: UIImage, indexPath: IndexPath) {
+        updateImageCount += 1
+    }
+ 
+}
